125259fbc9ae614487b33c56c741ed0cce257422
minseok7746 <minseok77746@naver.com>
2023-09-07 18:00:28 +0900

    feature: add game

---
+++ Frontend/ssahome/src/containers/game/RightSideBar/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './page.module.css'
+
+const RightSideBar = () => {
+  return (
+    <div>
+      오른쪽 사이드바
+    </div>
+  );
+};
+
+export default RightSideBar;
