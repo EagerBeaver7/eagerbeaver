06a08675a0640da987a3e00aec9d4a768c4b0a19
dener8 <8dener@gmail.com>
2023-09-12 12:47:00 +0900

    [BE] feat: swagger3 틀 구현

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/SwaggerConfig.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/SwaggerConfig.java
@@ -2,23 +2,41 @@ package ssafy.eagerbeaver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+// @Configuration
+// @EnableSwagger2
+// public class SwaggerConfig {
+// 	@Bean
+// 	public Docket api() {
+// 		return new Docket(DocumentationType.SWAGGER_2)
+// 			.select()
+// 			.apis(RequestHandlerSelectors.basePackage("ssafy.eagerbeaver.controller")) // API 컨트롤러 패키지 설정
+// 			.paths(PathSelectors.any())
+// 			.build()
+// 			.apiInfo(new ApiInfoBuilder().version("1.0").title("API Documentation").description("API Documentation").build());
+// 	}
+// }
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.example.controller")) // API 컨트롤러 패키지 설정
-			.paths(PathSelectors.any())
-			.build()
-			.apiInfo(new ApiInfoBuilder().version("1.0").title("API Documentation").description("API Documentation").build());
+	public OpenAPI OpenAPI() {
+		return new OpenAPI()
+			.info(new Info().title("eagerbeaver")
+				.description("eagerbeaver description")
+				.version("v0.0.1")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+			.externalDocs(new ExternalDocumentation()
+				.description("SpringShop Wiki Documentation")
+				.url("https://springshop.wiki.github.org/docs"));
 	}
 }
--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/controller/ExampleController.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/controller/ExampleController.java
@@ -1,18 +1,19 @@
 package ssafy.eagerbeaver.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Api(tags = "Sample Controller", description = "Sample API operations")
+@Tag(name = "Sample Controller", description = "Sample API operations")
 public class ExampleController {
 
 	@GetMapping("/hello")
-	@ApiOperation("Get a greeting message")
+	@Operation(description = "Get a greeting message")
 	public String getGreeting() {
 		return "Hello, Swagger!";
 	}
