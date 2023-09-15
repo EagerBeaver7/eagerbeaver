de840368400421d300da57076f04b56f5bfbf93b
dener8 <8dener@gmail.com>
2023-09-15 16:29:50 +0900

    feat: secret key 관리 WIP

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/EagerBeaverApplication.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/EagerBeaverApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import ssafy.eagerbeaver.util.JwtUtil;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class EagerBeaverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EagerBeaverApplication.class, args);
+
+		JwtUtil jwtUtil = new JwtUtil();
+		String jwt = jwtUtil.generateJwt("eagerbeaver@kakao.com");
+		System.out.println(jwt);
 	}
 
 }
--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/util/JwtUtil.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/util/JwtUtil.java
@@ -1,6 +1,8 @@
 package ssafy.eagerbeaver.util;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -22,54 +24,55 @@ public class JwtUtil {
 	private static final String AUTHORIZATION_HEADER = "Authorization";
 	private static final String BEARER_PREFIX = "Bearer ";
 	private static final long EXPIRATION_TIME = 60 * 60 * 1000L;
-	private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+	private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
 
-	@Value("${jwt.secretKey}")
-	private String secretKey;
+	// @Value("${jwt.secret}")
+	// private String secretKey;
 
 	public String generateJwt(String email) {
 		Date expirationDate = new Date(System.currentTimeMillis() + EXPIRATION_TIME);
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("email", email);
 
+		String secretKey = "eagerbeaversecretkeyhihellochillout";
 		return Jwts.builder()
-			.claim("email", email) // claim 추가할 경우 수정 필요
+			.setClaims(claims)
+			// .claim("email", email) // claim 추가할 경우 수정 필요
 			.setIssuedAt(new Date())
 			.setExpiration(expirationDate)
 			.signWith(SIGNATURE_ALGORITHM, secretKey)
 			.compact();
 	}
 
-	public String getJwtFromHeader(HttpServletRequest request) {
-		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-			return bearerToken.substring(7);
-		}
-		return null;
-	}
-
-	public boolean validateJwt(String jwt) {
-		try {
-			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody();
-			return true;
-		} catch (MalformedJwtException | SecurityException e) {
-			log.error("Invalid JWT signature");
-		} catch (ExpiredJwtException e) {
-			log.error("Expired JWT token");
-		} catch (UnsupportedJwtException e) {
-			log.error("Unsupported JWT token");
-		} catch (IllegalArgumentException e) {
-			log.error("Empty JWT token");
-		}
-		return false;
-	}
+	// public String getJwtFromHeader(HttpServletRequest request) {
+	// 	String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+	//
+	// 	if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+	// 		return bearerToken.substring(7);
+	// 	}
+	// 	return null;
+	// }
+	//
+	// public boolean validateJwt(String jwt) {
+	// 	try {
+	// 		Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody();
+	// 		return true;
+	// 	} catch (MalformedJwtException | SecurityException e) {
+	// 		log.error("Invalid JWT signature");
+	// 	} catch (ExpiredJwtException e) {
+	// 		log.error("Expired JWT token");
+	// 	} catch (UnsupportedJwtException e) {
+	// 		log.error("Unsupported JWT token");
+	// 	} catch (IllegalArgumentException e) {
+	// 		log.error("Empty JWT token");
+	// 	}
+	// 	return false;
+	// }
+	//
+	// public String getEmailFromJwt(String jwt) {
+	// 	return Jwts.parser()
+	// 		.setSigningKey(secretKey)
+	// 		.parseClaimsJws(jwt)
+	// 		.getBody().get("email", String.class);
+	// }
 }
-
-
-
-
-
-
-
-
-
-
