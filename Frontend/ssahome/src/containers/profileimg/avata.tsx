f1996912ffaa2ec15c0b6e6acfd1d74662f04720
서수정 <ssj946@naver.com>
2023-09-20 11:08:40 +0900

    fix: jenkinsfile

--- Frontend/ssahome/src/containers/profileimg/avata.tsx
+++ Frontend/ssahome/src/containers/profileimg/avata.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 import { Button, ButtonGroup, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import styles from './page.module.css';
 import Image from 'next/image';
-import beaver1 from "../../../public/images/IMG_0706.png";
-import beaver2 from "../../../public/images/IMG_0707.png";
-import beaver3 from "../../../public/images/IMG_0708.png";
-import beaver4 from "../../../public/images/IMG_0709.png";
-import beaver5 from "../../../public/images/IMG_0710.png";
-import beaver6 from "../../../public/images/IMG_0711.png";
+import beaver1 from "/images/IMG_0706.png";
+import beaver2 from "/images/IMG_0707.png";
+import beaver3 from "/images/IMG_0708.png";
+import beaver4 from "/images/IMG_0709.png";
+import beaver5 from "/images/IMG_0710.png";
+import beaver6 from "/images/IMG_0711.png";
 import Link from 'next/link';
 
 
