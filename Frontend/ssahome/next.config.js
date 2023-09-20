022d82ca52a8f7222459941f3eb48c4623e8cde6
서수정 <ssj946@naver.com>
2023-09-20 11:23:52 +0900

    fix: error

--- Frontend/ssahome/next.config.js
+++ Frontend/ssahome/next.config.js
@@ -1,8 +1,7 @@
 // next.config.js
 module.exports = {
     images: {
-      domains: ['j9a507.p.ssafy.io'], // 이미지 호스트 추가
-      loader: 'default', // 또는 'imgix'
+      domains: [], // 이미지 호스트 추가
     },
   };
   
