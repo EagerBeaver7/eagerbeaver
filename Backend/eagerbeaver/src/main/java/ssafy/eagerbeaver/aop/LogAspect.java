7277a8b2c8974f22256dfbf36fc2eae288c31a9e
Jeong TaeHee <th1563@naver.com>
2023-09-26 02:06:51 +0900

    feat: AOP 활용한 Logging 메서드 구현 -> @Log 어노테이션 생성, LogAspect 구현

---
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/aop/LogAspect.java
@@ -0,0 +1,32 @@
+package ssafy.eagerbeaver.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class LogAspect {
+
+    @Before("@annotation(Log)")
+    public void logBefore(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        String methodName = joinPoint.getSignature().toShortString();
+        log.info("[Before Log] {} args={}", methodName, args);
+    }
+    
+    @After("@annotation(Log)")
+    public void logAfter(JoinPoint joinPoint) {
+        String methodName = joinPoint.toLongString();
+        log.info("[After Log] {}", methodName);
+    }
+
+    @AfterThrowing(pointcut = "@annotation(Log)", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Exception exception) {
+        String methodName = joinPoint.getSignature().toShortString();
+        log.error("[Exception log] {} exception={}", methodName, exception.getMessage());
+    }
+}
