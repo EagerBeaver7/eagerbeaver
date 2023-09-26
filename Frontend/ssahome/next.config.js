c5127669d0e667ccd7c3c92fe692ab9a999cd7f5
서수정 <ssj946@naver.com>
2023-09-26 15:16:34 +0900

    dont

--- Frontend/ssahome/next.config.js
+++ Frontend/ssahome/next.config.js
@@ -1,7 +1,17 @@
 // next.config.js
- const nextConfig = {
+const nextConfig = {
     images: {
       domains: ['images.unsplash.com'], // 이미지 호스트 추가
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrutes() {
+      return [
+        {
+          source: '/:path*',
+          destination: 'http://localhost:8080/:path*'
+        }
+      ]  
     },
   };
   
