8341c8ad8168aaba92c7875f4e061e5a1b33a604
서수정 <ssj946@naver.com>
2023-09-26 10:49:23 +0900

    fix: port

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/WebConfig.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOriginPatterns("*")
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true)
