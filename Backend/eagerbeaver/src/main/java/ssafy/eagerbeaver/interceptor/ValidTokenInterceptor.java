10da35f0c24f6461944214a2479e8207aa237fae
dener8 <8dener@gmail.com>
2023-09-27 10:32:09 +0900

    feat: backend temp branch

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/interceptor/ValidTokenInterceptor.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/interceptor/ValidTokenInterceptor.java
@@ -1,11 +1,15 @@
 package ssafy.eagerbeaver.interceptor;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
+import java.util.Objects;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 import ssafy.eagerbeaver.util.JwtUtil;
 import ssafy.eagerbeaver.util.UserContextHolder;
 
@@ -13,12 +17,15 @@ import ssafy.eagerbeaver.util.UserContextHolder;
 @Slf4j
 public class ValidTokenInterceptor implements HandlerInterceptor {
 
-	private final JwtUtil jwtUtil = new JwtUtil();
+	private JwtUtil jwtUtil = new JwtUtil();
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws
-		Exception {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
 		log.info("interceptor 호출");
+
+		if (isPreflightRequest(request)) {
+			return true;
+		}
 
 		String jwt = jwtUtil.getJwtFromHeader(request);
 		jwtUtil.validateToken(jwt);
@@ -28,8 +35,28 @@ public class ValidTokenInterceptor implements HandlerInterceptor {
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-		Exception ex) {
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 		UserContextHolder.userIdHolder.remove();
 	}
+
+	private boolean isPreflightRequest(HttpServletRequest request) {
+		return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+	}
+
+	private boolean isOptions(HttpServletRequest request) {
+		return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+	}
+
+	private boolean hasHeaders(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+	}
+
+	private boolean hasMethod(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+	}
+
+	private boolean hasOrigin(HttpServletRequest request) {
+		return Objects.nonNull(request.getHeader("Origin"));
+	}
+
 }
