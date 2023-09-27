c8e890d36ba62c954efcb9204c0c28963a530769
dener8 <8dener@gmail.com>
2023-09-27 10:14:57 +0900

    feat: add preflight

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/interceptor/ValidTokenInterceptor.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/interceptor/ValidTokenInterceptor.java
@@ -1,10 +1,13 @@
 package ssafy.eagerbeaver.interceptor;
 
+import java.util.Objects;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import ssafy.eagerbeaver.util.JwtUtil;
@@ -20,9 +23,11 @@ public class ValidTokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
         log.info("interceptor 호출");
 
-        System.out.println("인터셉터 리퀘스트입니다: "+request.getHeader("Authorization"));
+        if (isPreflightRequest(request)) {
+            return true;
+        }
+
         String jwt = jwtUtil.getJwtFromHeader(request);
-        System.out.println("인터셉터 jwt입니다: "+jwt);
         jwtUtil.validateToken(jwt);
         // short id = jwtUtil.getIdFromJwt(jwt);
         UserContextHolder.userIdHolder.set(jwtUtil.getIdFromJwt(jwt));
@@ -33,4 +38,25 @@ public class ValidTokenInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         UserContextHolder.userIdHolder.remove();
     }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
+    }
+
 }
