e4aebbeb387da15c8342d9c74f786de8d999bd54
minseok7746 <minseok77746@naver.com>
2023-09-27 14:23:23 +0900

    feat: fix gamepage-gamelog

--- Frontend/ssahome/src/containers/game/RightSideBar/PurchaseList/page.tsx
+++ Frontend/ssahome/src/containers/game/RightSideBar/PurchaseList/page.tsx
@@ -37,6 +37,7 @@ type PurchaseListProps = {
 const PurchaseList: React.FC<PurchaseListProps> = ({ purchasedRegions, setPurchasedRegions, selectedRegion, currentPrices, seedMoney, setSeedMoney }) => {
   const [displayedQuantities, setDisplayedQuantities] = useState<number[]>([]);
 
+  
   // purchasedRegions가 변경될 때마다 displayedQuantities 업데이트
   useEffect(() => {
     setDisplayedQuantities(purchasedRegions.map(region => region.maxPurchaseNum));
@@ -45,7 +46,7 @@ const PurchaseList: React.FC<PurchaseListProps> = ({ purchasedRegions, setPurcha
   const incrementQuantity = (id: number) => {
     // id에 해당하는 지역의 현재 수량을 가져옴
     const currentQuantity = displayedQuantities[id - 1];
-    const maxQuantity = purchasedRegions[id - 1].maxPurchaseNum; // 최대 수량은 해당 지역의 maxPurchaseNum
+    const maxQuantity = purchasedRegions[id-1].maxPurchaseNum; // 최대 수량은 해당 지역의 maxPurchaseNum
   
     // 현재 수량이 최대 수량보다 작을 때만 증가
     if (currentQuantity < maxQuantity) {
@@ -73,37 +74,53 @@ const PurchaseList: React.FC<PurchaseListProps> = ({ purchasedRegions, setPurcha
     }
   };
   
-  const handleIncreaseHomePurchase = (id: number) => {
-    // id에 해당하는 지역의 현재 수량을 가져옴
+  const handleIncreaseHomePurchase = (id: number, currentPrice:number, maxPurchaseNum: number) => {
+    // id에 해당하는 지역의 현재 수량 (maxPurchaseNum)
+
+    // Item box 안에 있는 판매하고 싶은 수량
     const currentQuantity = displayedQuantities[id - 1];
-    const purchasePrice = purchasedRegions[id - 1].currentprice;
-  
+
     if (currentQuantity > 0) {
+      const remainingQuantity = maxPurchaseNum - currentQuantity; // 남은 개수
+
       // 판매할 아이템이 있을 때만 아래 작업 수행
-      const totalPrice = seedMoney + purchasePrice * currentQuantity;
-      console.log(totalPrice);
+      const totalPrice = seedMoney + currentPrice * currentQuantity;
   
       // 판매한 아이템 가격을 seedMoney에 추가
       setSeedMoney(totalPrice);
   
-      // 판매한 아이템의 수량을 0으로 설정
+      // 판매한 아이템의 수량을 0으로 설정  
       setDisplayedQuantities((prevQuantities) => {
         const newQuantities = [...prevQuantities];
-        newQuantities[id - 1] = 0;
+        newQuantities[id - 1] = maxPurchaseNum - currentQuantity;
         return newQuantities;
       });
-    }
+
+      // 남은 개수가 0이 되면 purchasedRegions에서 해당 지역 정보를 삭제
+      if (remainingQuantity === 0) {
+        setPurchasedRegions((prevRegions) => {
+          const newRegions = [...prevRegions];
+          newRegions.splice(id - 1, 1); // 해당 지역 정보 삭제
+          console.log(id)
+          return newRegions.map((region, index) => ({ ...region, id: index + 1 }));
+        });
+          
+      } else {
+        // 남은 개수가 0이 아니면 purchasedRegions를 업데이트하여 남은 개수를 반영
+        setPurchasedRegions((prevRegions) => {
+          const newRegions = [...prevRegions];
+          newRegions[id - 1] = { ...newRegions[id - 1], maxPurchaseNum: remainingQuantity };
+          return newRegions;
+        });
+      }
+    } 
+
   };
-  
-
-
-  
-  
   
   return (
     <div className={styles.PurchaseList}>
       <div className={styles.wrap}>
-        {purchasedRegions.map((region, id) => {
+        {purchasedRegions.map((region, index) => {
           // region.name에 해당하는 현재 가격을 currentPrice 변수에 가져옴
           const currentPrice = (currentPrices as Record<string, number>)[region.name] || 0;  // 턴 마다 지역의 현재 가격을 담고있음
 
@@ -119,7 +136,7 @@ const PurchaseList: React.FC<PurchaseListProps> = ({ purchasedRegions, setPurcha
                 <h4>개수: {region.maxPurchaseNum}</h4>  
               </div>
               <div className={styles.ButtonWrap}>
-                <Item>{displayedQuantities[(region.id)-1]}</Item>
+                <Item>{displayedQuantities[index]}</Item>
                 <div className={styles.IconButtonWrap}>
                   <IconButton
                     aria-label="plus"
@@ -136,7 +153,7 @@ const PurchaseList: React.FC<PurchaseListProps> = ({ purchasedRegions, setPurcha
                     <ArrowDropDownIcon fontSize="large"/>
                   </IconButton>
                 </div>
-                <button type="button" className={styles.button} onClick={() => handleIncreaseHomePurchase(region.id)}>
+                <button type="button" className={styles.button} onClick={() => handleIncreaseHomePurchase(region.id, currentPrice, region.maxPurchaseNum)}>
                   판매
                 </button>
               </div>
