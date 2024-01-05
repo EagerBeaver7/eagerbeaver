417d39334f8b19bd7ba6ca654ac3e1e3afed30e7
dener8 <8dener@gmail.com>
2024-01-05 18:13:57 +0900

    feat: 인터셉터 임시 해제

--- Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/WebConfig.java
+++ Backend/eagerbeaver/src/main/java/ssafy/eagerbeaver/config/WebConfig.java
@@ -12,10 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new ValidTokenInterceptor())
-			.addPathPatterns("/**")
-			.excludePathPatterns("/auth/**", "/nickname/**", "/word");
-		//            .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs", "/swagger-ui/index.html") // swagger 사용 미정
+//		registry.addInterceptor(new ValidTokenInterceptor())
+//			.addPathPatterns("/**")
+//			.excludePathPatterns("/auth/**", "/nickname/**", "/word");
+//		//            .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs", "/swagger-ui/index.html") // swagger 사용 미정
 	}
 
 	@Override
